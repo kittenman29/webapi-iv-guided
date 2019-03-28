b1c0dd556bf5a045d1f7863c10301b9f290af2e2
Will Schulz <will.schulz29@gmail.com>
2019-03-28 13:50:59 -0400

    changes

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 4000;
-const greeting = process.env.GREETING;
+// const greeting = process.env.GREETING;
 
 server.listen(port, () => {
-  console.log(`\*** ${greeting}, Server Running on http://localhost:${port} ***\n`);
+  console.log(`\***Server Running on http://localhost:${port} ***\n`);
 });
 
 // some changes here maybe
